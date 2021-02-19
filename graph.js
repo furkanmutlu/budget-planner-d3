0a30cc43509ebf969761799f77e4d79e72bf807e
Furkan Mutlu <furkanmutluu@gmail.com>
2021-02-19 09:09:21 +0100

    Arc generator and real time data setup

--- graph.js
+++ graph.js
@@ -13,8 +13,37 @@ const pie = d3.pie()
     .sort(null)
     .value(d => d.cost);
 
-const angles = pie([
-    { name: 'rent', cost: 500 },
-    { name: 'bills', cost: 300 },
-    { name: 'gaming', cost: 200 }
-]);
+const arcPath = d3.arc()
+    .outerRadius(dims.radius)
+    .innerRadius(dims.radius / 2);
+
+// Update function
+const update = (data) => {
+    console.log(data);
+}
+
+// Data Array and Firestore
+let data = [];
+
+db.collection('expenses').onSnapshot(res => {
+    res.docChanges().forEach(change => {
+        const doc = {...change.doc.data(), id: change.doc.id};
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc)
+                break;
+            case 'modified':
+                const index = data.findIndex(item => item.id === doc.id);
+                data[index] = doc;
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id);
+                break;
+            default:
+                break;
+        }
+    });
+
+    update(data);
+});
